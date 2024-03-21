18ca0c2471b2ddd0025ec3d700138b8f81d51339
shuyu36 <sharon87306@gmail.com>
2024-03-21 17:14:53 +0800

    240331

--- js/index_swiper.js
+++ js/index_swiper.js
@@ -1,24 +1,26 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1, // 設置每次顯示幾張幻燈片
-  loop: true, // 啟用循環
-  autoplay: {
-    delay: 3000, // 自動播放的間隔時間，以毫秒為單位
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  breakpoints: {
-    // 螢幕寬度小於等於 768px 時，顯示 1 張幻燈片
-    576: {
-      slidesPerView: 2,
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1, // 設置每次顯示幾張幻燈片
+    loop: true, // 啟用循環
+    autoplay: {
+      delay: 3000, // 自動播放的間隔時間，以毫秒為單位
     },
-    // 螢幕寬度小於等於 1024px 時，顯示 2 張幻燈片
-    1024: {
-      slidesPerView: 3,
+    pagination: {
+      el: ".swiper-pagination",
     },
-    // 螢幕寬度小於等於 1440px 時，顯示 3 張幻燈片（與原始設定相同）
-    1440: {
-      slidesPerView: 3,
+    breakpoints: {
+      // 螢幕寬度小於等於 768px 時，顯示 1 張幻燈片
+      576: {
+        slidesPerView: 2,
+      },
+      // 螢幕寬度小於等於 1024px 時，顯示 2 張幻燈片
+      1024: {
+        slidesPerView: 3,
+      },
+      // 螢幕寬度小於等於 1440px 時，顯示 3 張幻燈片（與原始設定相同）
+      1440: {
+        slidesPerView: 3,
+      },
     },
-  },
+  });
 });
